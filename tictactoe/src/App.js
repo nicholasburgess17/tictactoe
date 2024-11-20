55387e3d062d0f0c003c5f16ee4d5cb85f43f3c0
nicholasburgess17 <nicholas.burgess13@gmail.com>
2024-11-19 20:12:41 -0800

    format doc, add handleclick function to board function

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -1,6 +1,6 @@
 import "./index.css";
 import { useState } from "react";
-function Square({ value , onSquareClick }) {
+function Square({ value, onSquareClick }) {
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
@@ -9,10 +9,15 @@ function Square({ value , onSquareClick }) {
 }
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  function handleClick() {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
   return (
     <>
       <div className="board-row">
-      <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[0]} onSquareClick={handleClick} />
         <Square value={squares[1]} onSquareClick={handleClick} />
         <Square value={squares[2]} onSquareClick={handleClick} />
       </div>
