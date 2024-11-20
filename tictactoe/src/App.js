20bfa86653910ca7fc57d130cf60e7d3c771b226
nicholasburgess17 <nicholas.burgess13@gmail.com>
2024-11-19 21:19:54 -0800

    add determining winner

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -10,6 +10,7 @@ function Square({ value, onSquareClick }) {
 }
 // Sets the board
 export default function Board() {
+  
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
   // Handles clicking on squares, marking them with an x
@@ -47,4 +48,24 @@ export default function Board() {
       </div>
     </>
   );
+  //determines winner
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 }
